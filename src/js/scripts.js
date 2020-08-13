46b030f23f94dc619c0722f56e06f238ea3682b2
Prinz Nikko <nikko@webforest.solutions>
2020-08-13 19:56:46 +0800

    Increase offset value when steps are triggered

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -104,7 +104,7 @@
 					}
 				} else if ($('body.single-works').length > 0){
 					var scroll = $(window).scrollTop();
-					var pineProcess = $(".pine-process").offset().top - 600;
+					var pineProcess = $(".pine-process").offset().top - 900;
 					var width = $(window).width();
 					if (scroll >= pineProcess && scroll <= (pineProcess + $(".pine-process").height()) && width > 768 ) {
 							// enableScroll = false;
